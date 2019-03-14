61b3e584f3c1343ffe5bb573eace96d57a2e270d
Andriy Blakitniy <41573304+homzilla@users.noreply.github.com>
2019-03-14 20:50:32 +0200

    tests
    add exsisting tests

---
+++ UALTESTS/Tests bat/ual_r01_ws00_ads.bat
@@ -0,0 +1,9 @@
+@rem Project: ualtests
+@rem BuildNo: 115
+@rem Type: debug
+@rem Date: 15.02.2019 12:07:39
+@rem Changeset: 0
+@rem User: Administrator
+@rem Workstation: U7_TEST
+
+AppDataSrv.exe -e -cfgip1=127.0.0.1:13312 -cfgip2=127.0.0.1:13312 -id=UAL_R01_WS00_ADS
---
+++ UALTESTS/Tests bat/ual_r01_ws00_cfgs.bat
@@ -0,0 +1,9 @@
+@rem Project: ualtests
+@rem BuildNo: 115
+@rem Type: debug
+@rem Date: 15.02.2019 12:07:39
+@rem Changeset: 0
+@rem User: Administrator
+@rem Workstation: U7_TEST
+
+CfgSrv.exe -e -id=UAL_R01_WS00_CFGS -b=C:/Users/Workstation/AppData/Local/ualtests-debug/build -ip=127.0.0.1:13312
